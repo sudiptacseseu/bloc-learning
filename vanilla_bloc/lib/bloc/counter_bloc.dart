548fbe41e17ac96ef000bb44719ded2fbe240200
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-09-17 21:14:18 +0600

    Broadcaster stream controller added

--- vanilla_bloc/lib/bloc/counter_bloc.dart
+++ vanilla_bloc/lib/bloc/counter_bloc.dart
@@ -8,7 +8,12 @@ import 'counter_event.dart';
 class CounterBloc {
   int _counter = 0;
 
+  // Single subscription stream controller that provides only one listener
   final _stateStreamController = StreamController<int>();
+
+  // Broadcast stream controller that provide multiple listeners
+  // final _stateStreamController = StreamController<int>.broadcast();
+
   StreamSink<int> get _counterSink => _stateStreamController.sink;
   // For state, exposing only a stream which outputs data and this can't be private
   Stream<int> get counterStream => _stateStreamController.stream;
@@ -18,6 +23,8 @@ class CounterBloc {
   Sink<CounterEvent> get counterEventSink => _eventStreamController.sink;
 
   CounterBloc() {
+    // Multiple listener for state stream controller, one is from stream builder and another one is this one
+    // _stateStreamController.stream.listen((event) {});
     // Continuously listening, whenever there is a new event, we want to map it to a new state
     _eventStreamController.stream.listen(_mapEventToState);
   }
